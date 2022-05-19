64557e4a806dcbba0c7f4f6735f636cd7f0f0147
prasanna lakshmi <prasannalakshmikathi@gmail.com>
2022-05-19 18:22:19 +0530

    [prasanna]: calculate percentage of flipcoin

---
+++ functionalprograms/functionalprograms/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+functionalprograms.flipcoin value = new functionalprograms.flipcoin();
+value.flipcoin();
---
+++ functionalprograms/functionalprograms/flipcoin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functionalprograms
+{
+    internal class flipcoin
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's flip a coin 50 times");
+            Coinflip();
+        }
+
+        private static int Coinflip()
+        {
+            Random Rand = new Random();
+            int heads = 0;
+            int tails = 0;
+            int headspercentage = 0;
+            int tailspercentage = 0;
+            int result = 0;
+
+
+            for (int i = 0; i < 50; i++)
+            {
+                result = Rand.Next(0, 2);
+                headspercentage = heads / 5;
+                tailspercentage = 50 - headspercentage;
+
+                if (result == 1)
+                {
+                    heads++;
+                }
+                else
+                {
+                    tails++;
+                }
+
+            }
+            Console.WriteLine("heads was flipped {0} times", heads);
+            Console.WriteLine("tails was flipped  {0} times", tails);
+            Console.WriteLine("Heads Percentage : " + (headspercentage));
+            Console.WriteLine("Tails Percentage :" + (tailspercentage));
+            return result;
+        }
+    }
+}
