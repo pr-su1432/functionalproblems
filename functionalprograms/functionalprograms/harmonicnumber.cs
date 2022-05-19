833a161a9f2c118c6b0bd94a955b8009c17bf63f
prasanna lakshmi <prasannalakshmikathi@gmail.com>
2022-05-19 19:59:51 +0530

    [prasanna]: calculate harmonic numbers

---
+++ functionalprograms/functionalprograms/harmonicnumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functionalprograms
+{
+    class hormonicnumber
+    {
+
+        // Function to find N-th Harmonic Number
+        static double nthHarmonic(int N)
+        {
+            // H1 = 1
+            float harmonic = 1;
+
+            // loop to apply the formula
+            // Hn = H1 + H2 + H3 ... +
+            // Hn-1 + Hn-1 + 1/n
+            for (int i = 2; i <= N; i++)
+            {
+                harmonic += (float)1 / i;
+            }
+
+            return harmonic;
+        }
+
+        // Driver Code
+        static public void Main()
+        {
+            int N = 8;
+
+            Console.Write(nthHarmonic(N));
+        }
+    }
+
+
+
+}
