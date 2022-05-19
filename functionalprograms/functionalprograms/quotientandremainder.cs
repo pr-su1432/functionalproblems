d4798a6106c1b29cafcdc097452221d01c4a99a3
prasanna lakshmi <prasannalakshmikathi@gmail.com>
2022-05-19 20:57:52 +0530

    [prasanna]: calculating remainder

---
+++ functionalprograms/functionalprograms/quotientandremainder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functionalprograms
+{
+    public class quotientandremainder
+    {
+        public static void Main()
+        {
+            int dividend = 30;
+            int divisor = 7;
+            int remainder;
+            int quotient = Math.DivRem(dividend, divisor, out remainder);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+        }
+    }
+}
